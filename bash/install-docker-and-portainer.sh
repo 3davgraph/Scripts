f575f99c44a365aa145cfa0442323170280d41ee
3davgraph <85966297+3davgraph@users.noreply.github.com>
2023-01-25 21:35:16 -0500

    Update install-docker-and-portainer.sh
    adding portainer install

--- bash/install-docker-and-portainer.sh
+++ bash/install-docker-and-portainer.sh
@@ -8,4 +8,6 @@ curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo gpg --dearmor -o 
 && \
 echo "deb [arch=$(dpkg --print-architecture) signed-by=/etc/apt/keyrings/docker.gpg] https://download.docker.com/linux/ubuntu $(lsb_release -cs) stable" | sudo tee /etc/apt/sources.list.d/docker.list > /dev/null && \
  sudo apt-get update  && \
- sudo apt-get install -y docker-ce docker-ce-cli containerd.io docker-compose-plugin
+ sudo apt-get install -y docker-ce docker-ce-cli containerd.io docker-compose-plugin && \
+ sudo docker volume create portainer_data && \
+docker run -d -p 8000:8000 -p 9443:9443 --name portainer --restart=always -v /var/run/docker.sock:/var/run/docker.sock -v portainer_data:/data portainer/portainer-ce:latest
