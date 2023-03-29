84a0a9f4e2adaf62ae000ac1d1d87b9310c3ef96
3davgraph <85966297+3davgraph@users.noreply.github.com>
2023-03-28 20:35:08 -0400

    simplified and updated script using chatgpt

--- bash/install-docker-and-portainer.sh
+++ bash/install-docker-and-portainer.sh
@@ -1,24 +1,17 @@
-#!/bin/bash 
-# this will install Docker and then Portainer. These were copied and compiled together from the offical websites.
- 
- # First update and install the needed files
- sudo apt-get update && sudo apt-get install \
-    ca-certificates \
-    curl \
-    gnupg \
-    lsb-release && \
-# Next we need to add Docker's keyring
-sudo mkdir -p /etc/apt/keyrings && \
-curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo gpg --dearmor -o /etc/apt/keyrings/docker.gpg && \
+#!/bin/bash
 
-# Now we add Docker's repo to docker.list in /etc/apt/sources.list.d/
-echo "deb [arch=$(dpkg --print-architecture) signed-by=/etc/apt/keyrings/docker.gpg] https://download.docker.com/linux/ubuntu $(lsb_release -cs) stable" | sudo tee /etc/apt/sources.list.d/docker.list > /dev/null && \
- # update again
- sudo apt-get update  && \
- #install Docker (finally)
- sudo apt-get install -y docker-ce docker-ce-cli containerd.io docker-compose-plugin && \
- #Now we add Portainer CE (Commumity Edition) to a standalone Docker Instalation (1 Computer)
- sudo docker volume create portainer_data && \
- sudo docker run -d -p 8000:8000 -p 9443:9443 --name portainer --restart=always -v /var/run/docker.sock:/var/run/docker.sock -v portainer_data:/data portainer/portainer-ce:latest
-# now visits - [IP OF COMPUTER WITH DOCKER]:9443 if accessing remotly. (Make sure port 9443 is open through the firewall)
-#If accessing localy, visit - localhost:9443
+# Update and install required packages
+sudo apt-get update
+sudo apt-get install -y curl gnupg lsb-release
+
+# Add Docker's keyring and repository
+curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo gpg --dearmor -o /usr/share/keyrings/docker-archive-keyring.gpg
+echo "deb [arch=$(dpkg --print-architecture) signed-by=/usr/share/keyrings/docker-archive-keyring.gpg] https://download.docker.com/linux/ubuntu $(lsb_release -cs) stable" | sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
+
+# Update and install Docker
+sudo apt-get update
+sudo apt-get install -y docker-ce docker-ce-cli containerd.io docker-compose
+
+# Create Portainer data volume and run Portainer
+sudo docker volume create portainer_data
+sudo docker run -d -p 8000:8000 -p 9443:9443 --name portainer --restart=always -v /var/run/docker.sock:/var/run/docker.sock -v portainer_data:/data portainer/portainer-ce:latest
