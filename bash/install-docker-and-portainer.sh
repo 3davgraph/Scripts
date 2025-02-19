edbed41a201015984a276d93bb162b1ce21e39fc
3davgraph <85966297+3davgraph@users.noreply.github.com>
2025-02-18 23:05:22 -0500

    Update install-docker-and-portainer.sh
    This is put here for notes currently and will not work untill fixed. 
    I needed all scripts I was going to use and needed in one place to edit later.

--- bash/install-docker-and-portainer.sh
+++ bash/install-docker-and-portainer.sh
@@ -1,5 +1,44 @@
 #!/bin/bash 
 # this will install Docker and then Portainer. These were copied and compiled together from the offical websites.
+
+This is put here for notes currently and will not work untill fixed. 
+I needed all scripts I was going to use and needed in one place to edit later.
+
+# Detect the distribution
+if [ -f /etc/os-release ]; then
+    . /etc/os-release
+    DISTRO=$ID
+else
+    echo "Unable to detect distribution."
+    exit 1
+fi
+
+# Choose the package manager based on the distribution
+if [[ "$DISTRO" == "centos" ]] || [[ "$DISTRO" == "rhel" ]]; then
+    distro="dnf"
+elif [[ "$DISTRO" == "ubuntu" ]] || [[ "$DISTRO" == "debian" ]]; then
+    distro="apt"
+else
+    echo "Unsupported distribution: $DISTRO"
+    exit 1
+fi
+
+sudo dnf remove docker \
+                  docker-client \
+                  docker-client-latest \
+                  docker-common \
+                  docker-latest \
+                  docker-latest-logrotate \
+                  docker-logrotate \
+                  docker-engine
+
+sudo dnf -y install dnf-plugins-core
+sudo dnf config-manager --add-repo https://download.docker.com/linux/centos/docker-ce.repo
+
+sudo dnf install docker-ce docker-ce-cli containerd.io docker-buildx-plugin docker-compose-plugin
+
+If prompted to accept the GPG key, verify that the fingerprint matches 060A 61C5 1B55 8A7F 742B 77AA C52F EB6B 621E 9F35, and if so, accept it.
+
  
 # First update and install the needed files
 sudo apt-get update
